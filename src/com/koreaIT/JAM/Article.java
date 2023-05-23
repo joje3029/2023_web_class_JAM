efc4e2d99d800a68d898265b46c67deb1a65b48a
joje3029 <joje3029@gmail.com>
2023-05-23 14:31:45 +0900

    article write, list

---
+++ src/com/koreaIT/JAM/Article.java
@@ -0,0 +1,12 @@
+package com.koreaIT.JAM;
+
+public class Article {
+	int id;
+	String title;
+	String body;
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.body = body;
+		this.title = title;
+	}
+}
--- src/com/koreaIT/JAM/Main.java
+++ src/com/koreaIT/JAM/Main.java
@@ -1,5 +1,7 @@
 package com.koreaIT.JAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +9,10 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 
 		Scanner sc = new Scanner(System.in);
+		
+		List<Article> articles = new ArrayList<>();
+		
+		int lastArticledId = 0;
 		
 		while(true) {
 			System.out.printf("명령어) ");
@@ -17,6 +23,36 @@ public class Main {
 				break;
 			}
 			
+			if(cmd.equals("article write")) {
+				System.out.println("== 게시물 작성 ==");
+				
+				int id = lastArticledId;
+				lastArticledId = id;
+				
+				System.out.printf("제목) ");
+				String title = sc.nextLine();
+				System.out.printf("내용) ");
+				String body = sc.nextLine();
+				
+				Article article = new Article(id, title, body);
+				
+				articles.add(article);
+			
+				System.out.printf("%d번 게시물이 생성되었습니다.", id);
+			}else if(cmd.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==");
+				
+				if(articles.size()==0){
+					System.out.println("게시물이 작성되지 않았습니다.");
+					continue;
+				}
+				for(Article article : articles) {
+				System.out.printf("	번호	|	제목");
+				System.out.printf("	%d	| %s\n", article.id, article.title);
+			
+				}
+			
+			}
 			
 			
 		}
